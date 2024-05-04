7651c7607a80fba29dda1e7ca93c0a96138377a1
이대현 <ej4658@naver.com>
2024-05-05 00:45:28 +0900

    잘되는 데모 찾음

--- FullStack/sweaty/static/js/MoCap.js
+++ FullStack/sweaty/static/js/MoCap.js
@@ -255,13 +255,13 @@ const animateVRM = (vrm, results) => {
   const rightHandLandmarks = results.leftHandLandmarks;
 
   // Animate Face
-  // if (faceLandmarks) {
-  //   riggedFace = Kalidokit.Face.solve(faceLandmarks, {
-  //     runtime: "mediapipe",
-  //     video: videoElement,
-  //   });
-  //   rigFace(riggedFace);
-  // }
+  if (faceLandmarks) {
+    riggedFace = Kalidokit.Face.solve(faceLandmarks, {
+      runtime: "mediapipe",
+      video: videoElement,
+    });
+    rigFace(riggedFace);
+  }
 
   // Animate Pose
   if (pose2DLandmarks && pose3DLandmarks) {
@@ -346,72 +346,72 @@ const animateVRM = (vrm, results) => {
   }
 
   // Animate Hands
-  // if (leftHandLandmarks) {
-  //   riggedLeftHand = Kalidokit.Hand.solve(leftHandLandmarks, "Left");
-  //   rigRotation("LeftHand", {
-  //     // Combine pose rotation Z and hand rotation X Y
-  //     z: riggedPose.LeftHand.z,
-  //     y: riggedLeftHand.LeftWrist.y,
-  //     x: riggedLeftHand.LeftWrist.x,
-  //   });
-  //   rigRotation("LeftRingProximal", riggedLeftHand.LeftRingProximal);
-  //   rigRotation("LeftRingIntermediate", riggedLeftHand.LeftRingIntermediate);
-  //   rigRotation("LeftRingDistal", riggedLeftHand.LeftRingDistal);
-  //   rigRotation("LeftIndexProximal", riggedLeftHand.LeftIndexProximal);
-  //   rigRotation("LeftIndexIntermediate", riggedLeftHand.LeftIndexIntermediate);
-  //   rigRotation("LeftIndexDistal", riggedLeftHand.LeftIndexDistal);
-  //   rigRotation("LeftMiddleProximal", riggedLeftHand.LeftMiddleProximal);
-  //   rigRotation(
-  //     "LeftMiddleIntermediate",
-  //     riggedLeftHand.LeftMiddleIntermediate
-  //   );
-  //   rigRotation("LeftMiddleDistal", riggedLeftHand.LeftMiddleDistal);
-  //   rigRotation("LeftThumbProximal", riggedLeftHand.LeftThumbProximal);
-  //   rigRotation("LeftThumbIntermediate", riggedLeftHand.LeftThumbIntermediate);
-  //   rigRotation("LeftThumbDistal", riggedLeftHand.LeftThumbDistal);
-  //   rigRotation("LeftLittleProximal", riggedLeftHand.LeftLittleProximal);
-  //   rigRotation(
-  //     "LeftLittleIntermediate",
-  //     riggedLeftHand.LeftLittleIntermediate
-  //   );
-  //   rigRotation("LeftLittleDistal", riggedLeftHand.LeftLittleDistal);
-  // }
-  // if (rightHandLandmarks) {
-  //   riggedRightHand = Kalidokit.Hand.solve(rightHandLandmarks, "Right");
-  //   rigRotation("RightHand", {
-  //     // Combine Z axis from pose hand and X/Y axis from hand wrist rotation
-  //     z: riggedPose.RightHand.z,
-  //     y: riggedRightHand.RightWrist.y,
-  //     x: riggedRightHand.RightWrist.x,
-  //   });
-  //   rigRotation("RightRingProximal", riggedRightHand.RightRingProximal);
-  //   rigRotation("RightRingIntermediate", riggedRightHand.RightRingIntermediate);
-  //   rigRotation("RightRingDistal", riggedRightHand.RightRingDistal);
-  //   rigRotation("RightIndexProximal", riggedRightHand.RightIndexProximal);
-  //   rigRotation(
-  //     "RightIndexIntermediate",
-  //     riggedRightHand.RightIndexIntermediate
-  //   );
-  //   rigRotation("RightIndexDistal", riggedRightHand.RightIndexDistal);
-  //   rigRotation("RightMiddleProximal", riggedRightHand.RightMiddleProximal);
-  //   rigRotation(
-  //     "RightMiddleIntermediate",
-  //     riggedRightHand.RightMiddleIntermediate
-  //   );
-  //   rigRotation("RightMiddleDistal", riggedRightHand.RightMiddleDistal);
-  //   rigRotation("RightThumbProximal", riggedRightHand.RightThumbProximal);
-  //   rigRotation(
-  //     "RightThumbIntermediate",
-  //     riggedRightHand.RightThumbIntermediate
-  //   );
-  //   rigRotation("RightThumbDistal", riggedRightHand.RightThumbDistal);
-  //   rigRotation("RightLittleProximal", riggedRightHand.RightLittleProximal);
-  //   rigRotation(
-  //     "RightLittleIntermediate",
-  //     riggedRightHand.RightLittleIntermediate
-  //   );
-  //   rigRotation("RightLittleDistal", riggedRightHand.RightLittleDistal);
-  // }
+  if (leftHandLandmarks) {
+    riggedLeftHand = Kalidokit.Hand.solve(leftHandLandmarks, "Left");
+    rigRotation("LeftHand", {
+      // Combine pose rotation Z and hand rotation X Y
+      z: riggedPose.LeftHand.z,
+      y: riggedLeftHand.LeftWrist.y,
+      x: riggedLeftHand.LeftWrist.x,
+    });
+    rigRotation("LeftRingProximal", riggedLeftHand.LeftRingProximal);
+    rigRotation("LeftRingIntermediate", riggedLeftHand.LeftRingIntermediate);
+    rigRotation("LeftRingDistal", riggedLeftHand.LeftRingDistal);
+    rigRotation("LeftIndexProximal", riggedLeftHand.LeftIndexProximal);
+    rigRotation("LeftIndexIntermediate", riggedLeftHand.LeftIndexIntermediate);
+    rigRotation("LeftIndexDistal", riggedLeftHand.LeftIndexDistal);
+    rigRotation("LeftMiddleProximal", riggedLeftHand.LeftMiddleProximal);
+    rigRotation(
+      "LeftMiddleIntermediate",
+      riggedLeftHand.LeftMiddleIntermediate
+    );
+    rigRotation("LeftMiddleDistal", riggedLeftHand.LeftMiddleDistal);
+    rigRotation("LeftThumbProximal", riggedLeftHand.LeftThumbProximal);
+    rigRotation("LeftThumbIntermediate", riggedLeftHand.LeftThumbIntermediate);
+    rigRotation("LeftThumbDistal", riggedLeftHand.LeftThumbDistal);
+    rigRotation("LeftLittleProximal", riggedLeftHand.LeftLittleProximal);
+    rigRotation(
+      "LeftLittleIntermediate",
+      riggedLeftHand.LeftLittleIntermediate
+    );
+    rigRotation("LeftLittleDistal", riggedLeftHand.LeftLittleDistal);
+  }
+  if (rightHandLandmarks) {
+    riggedRightHand = Kalidokit.Hand.solve(rightHandLandmarks, "Right");
+    rigRotation("RightHand", {
+      // Combine Z axis from pose hand and X/Y axis from hand wrist rotation
+      z: riggedPose.RightHand.z,
+      y: riggedRightHand.RightWrist.y,
+      x: riggedRightHand.RightWrist.x,
+    });
+    rigRotation("RightRingProximal", riggedRightHand.RightRingProximal);
+    rigRotation("RightRingIntermediate", riggedRightHand.RightRingIntermediate);
+    rigRotation("RightRingDistal", riggedRightHand.RightRingDistal);
+    rigRotation("RightIndexProximal", riggedRightHand.RightIndexProximal);
+    rigRotation(
+      "RightIndexIntermediate",
+      riggedRightHand.RightIndexIntermediate
+    );
+    rigRotation("RightIndexDistal", riggedRightHand.RightIndexDistal);
+    rigRotation("RightMiddleProximal", riggedRightHand.RightMiddleProximal);
+    rigRotation(
+      "RightMiddleIntermediate",
+      riggedRightHand.RightMiddleIntermediate
+    );
+    rigRotation("RightMiddleDistal", riggedRightHand.RightMiddleDistal);
+    rigRotation("RightThumbProximal", riggedRightHand.RightThumbProximal);
+    rigRotation(
+      "RightThumbIntermediate",
+      riggedRightHand.RightThumbIntermediate
+    );
+    rigRotation("RightThumbDistal", riggedRightHand.RightThumbDistal);
+    rigRotation("RightLittleProximal", riggedRightHand.RightLittleProximal);
+    rigRotation(
+      "RightLittleIntermediate",
+      riggedRightHand.RightLittleIntermediate
+    );
+    rigRotation("RightLittleDistal", riggedRightHand.RightLittleDistal);
+  }
 };
 
 //AJAX 좌표 전송
